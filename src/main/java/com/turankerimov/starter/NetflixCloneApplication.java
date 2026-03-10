8c047fcd09d17757dca363d189ef3e13a2383ff4
Turan Kerimov <kerimovturan06@gmail.com>
2026-03-10 13:32:51 +0400

    First commit

---
+++ src/main/java/com/turankerimov/starter/NetflixCloneApplication.java
@@ -0,0 +1,19 @@
+package com.turankerimov.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EntityScan(basePackages ={"com.turankerimov"})
+@ComponentScan(basePackages = {"com.turankerimov"})
+@EnableJpaRepositories(basePackages = {"com.turankerimov"})
+@SpringBootApplication
+public class NetflixCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turankerimov/starter/NeflixCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turankerimov.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeflixCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
