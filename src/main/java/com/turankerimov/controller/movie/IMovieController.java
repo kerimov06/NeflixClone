64c930aae0675b61783bd746c0bdced4ee5123e3
Turan Kerimov <kerimovturan06@gmail.com>
2026-03-11 19:58:14 +0400

    Service sinifleri packaglere ayrildi

--- src/main/java/com/turankerimov/controller/movie/IMovieController.java
+++ src/main/java/com/turankerimov/controller/movie/IMovieController.java
@@ -1,4 +1,9 @@
 package com.turankerimov.controller.movie;
 
+import com.turankerimov.dto.movie.MovieDtoIU;
+import com.turankerimov.dto.movie.MovieResponseDto;
+
 public interface IMovieController {
+
+     public MovieResponseDto saveMovie(MovieDtoIU saveMovie);
 }
--- src/main/java/com/turankerimov/controller/movie/MovieControllerImpl.java
+++ src/main/java/com/turankerimov/controller/movie/MovieControllerImpl.java
@@ -1,10 +1,18 @@
 package com.turankerimov.controller.movie;
 
+import com.turankerimov.dto.movie.MovieDtoIU;
+import com.turankerimov.dto.movie.MovieResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 @RequestMapping("/mvoie")
-public class MovieControllerImpl {
+public class MovieControllerImpl implements IMovieController {
 
+
+    @Override
+    public MovieResponseDto saveMovie(MovieDtoIU saveMovie) {
+        return null;
+    }
 }
