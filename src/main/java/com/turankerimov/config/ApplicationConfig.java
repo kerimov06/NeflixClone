52ba99a1ba07082fac333a86fa95325e6ad543d4
Turan Kerimov <kerimovturan06@gmail.com>
2026-03-24 21:23:04 +0400

    AppConfig icersinde beanler elave olundu ve userRepo da username ile istifadeci tapmaq ucun metod yazildi

---
+++ src/main/java/com/turankerimov/config/ApplicationConfig.java
@@ -0,0 +1,49 @@
+package com.turankerimov.config;
+
+import com.turankerimov.entity.User;
+import com.turankerimov.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.util.Optional;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+
+     public UserDetailsService userDetailsService() {
+         return new UserDetailsService() {
+
+             @Override
+             public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                 return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+             }
+
+         };
+     }
+
+
+      @Bean
+      public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration ) {
+          return configuration.getAuthenticationManager();
+      }
+
+
+    @Bean
+    public BCryptPasswordEncoder  bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
--- src/main/java/com/turankerimov/repository/user/UserRepository.java
+++ src/main/java/com/turankerimov/repository/user/UserRepository.java
@@ -4,6 +4,12 @@ import com.turankerimov.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByUsername(String username);
+
+    void fnd();
 }
