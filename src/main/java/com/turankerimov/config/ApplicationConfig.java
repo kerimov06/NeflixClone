8aef7c7f49c8107587d867a64081cabaf68ea1af
Turan Kerimov <kerimovturan06@gmail.com>
2026-03-28 22:10:40 +0400

    Security config sinifinde confiqurasiyalar duzgun sekilde yazildi ve quruldu daha sonra app configde elave olunmayan bean elave edildi ve evvelki kooda kicik deyisklik olundu

--- src/main/java/com/turankerimov/config/ApplicationConfig.java
+++ src/main/java/com/turankerimov/config/ApplicationConfig.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -22,18 +19,24 @@ public class ApplicationConfig {
     @Autowired
     private UserRepository userRepository;
 
+
+    @Bean
      public UserDetailsService userDetailsService() {
-         return new UserDetailsService() {
+        return new UserDetailsService() {
 
-             @Override
-             public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                 return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                Optional<User> optional = userRepository.findByUsername(username);
+                if (optional.isPresent()) {
+                    return optional.get();
+                } else {
+                    throw new UsernameNotFoundException(username);
+                }
+            }
 
-             }
 
-         };
-     }
-
+        };
+    }
 
       @Bean
       public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration ) {
---
+++ src/main/java/com/turankerimov/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.turankerimov.config;
+
+import com.turankerimov.jwt.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    public static final String AUTHENTICATE = "/authenticate";
+    public static final String REGISTER = "/register";
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+          http
+                  .csrf(csrf -> csrf.disable())
+                  .cors(cors -> cors.disable())
+                  .sessionManagement(session ->
+                          session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                  .authorizeHttpRequests(request ->
+                          request.requestMatchers(AUTHENTICATE, REGISTER).permitAll().anyRequest().authenticated())
+                  .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+          return http.build();
+    }
+
+
+}
