82925ec08509bc1ffa852a4a2067aff2766bfac9
Turan Kerimov <kerimovturan06@gmail.com>
2026-04-03 23:25:06 +0400

    Xeta aldiqda 401 donmesi ucun autentrypoint elave etdim

--- src/main/java/com/turankerimov/config/SecurityConfig.java
+++ src/main/java/com/turankerimov/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.turankerimov.config;
 
+import com.turankerimov.jwt.AuthEntryPoint;
 import com.turankerimov.jwt.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -22,15 +24,22 @@ public class SecurityConfig {
     @Autowired
     private JwtAuthFilter jwtAuthFilter;
 
+    @Autowired
+    private AuthEntryPoint authEntryPoint;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
           http
                   .csrf(csrf -> csrf.disable())
                   .cors(cors -> cors.disable())
                   .sessionManagement(session ->
-                          session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                          session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                  ).exceptionHandling(expecption->
+                          expecption.authenticationEntryPoint(authEntryPoint)
+                  )
                   .authorizeHttpRequests(request ->
-                          request.requestMatchers(AUTHENTICATE, REGISTER).permitAll().anyRequest().authenticated())
+                          request.requestMatchers(AUTHENTICATE, REGISTER).permitAll().anyRequest().authenticated()
+                  )
                   .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
           return http.build();
